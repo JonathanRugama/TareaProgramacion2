3db0ece803dfd87c0c71e851eed20de48b868c72
Jonathan Rugama <jrugama96@gmail.com>
2018-07-03 20:49:55 -0600

    Implementación de llenado sin exito
    -No llena jugador 1
    -En algunos casos setea "BL" de un solo tiro

---
+++ RotarI/src/PuntuacionJugadores.java
@@ -0,0 +1,134 @@
+
+public class PuntuacionJugadores {
+
+	public char [] jugador1 =new char [8];
+	public char [] jugador2=new char [8];
+	public char [] jugador3=new char [8];
+	public char[]jugador4=new char [8];
+	public char[]jugador5=new char [8];
+	public char[]jugador6=new char [8];
+	public char[]jugador7=new char [8];
+	public char[]jugador8=new char [8];
+	public PuntuacionJugadores() {
+	}
+	
+	public String llenadoJugadores(int indiceJugadorALlenar, int indicePosicionALlenar, char []palabra ) {
+		String llenado=""; 
+		
+		switch (indiceJugadorALlenar) {
+		case 1:
+			for (int i= 0; i<indicePosicionALlenar; i++) {
+				
+				llenado += String.valueOf(palabra[i]);
+			}
+			
+			return llenado;
+		case 2: 
+      for (int i= 0; i<indicePosicionALlenar; i++) {
+				
+				llenado += String.valueOf(palabra[i]);
+			}
+			return llenado;
+			
+		case 3:
+		      for (int i= 0; i<indicePosicionALlenar; i++) {
+					
+					llenado += String.valueOf(palabra[i]);
+				}
+			return llenado;
+			
+		case 4:
+		      for (int i= 0; i<indicePosicionALlenar; i++) {
+					
+					llenado += String.valueOf(palabra[i]);
+				}
+			
+			return llenado;
+			
+		case 5:
+		      for (int i= 0; i<indicePosicionALlenar; i++) {
+					
+					llenado += String.valueOf(palabra[i]);
+				}
+			return llenado;
+			
+		case 6: 
+		      for (int i= 0; i<indicePosicionALlenar; i++) {
+					
+					llenado += String.valueOf(palabra[i]);
+				}
+			return llenado;
+		
+		case 7:
+		      for (int i= 0; i<indicePosicionALlenar; i++) {
+					
+					llenado += String.valueOf(palabra[i]);
+				}
+			return llenado;
+			
+		case 8: 
+		      for (int i= 0; i<indicePosicionALlenar; i++) {
+					
+					llenado += String.valueOf(palabra[i]);
+				}
+		  	return llenado;
+		default:
+		  	
+		}
+	
+		return llenado;
+	
+	}
+	
+	public char[] getJugador1() {
+		return jugador1;
+	}
+	public void setJugador1(char[] jugador1) {
+		this.jugador1 = jugador1;
+	}
+	public char[] getJugador2() {
+		return jugador2;
+	}
+	public void setJugador2(char[] jugador2) {
+		this.jugador2 = jugador2;
+	}
+	public char[] getJugador3() {
+		return jugador3;
+	}
+	public void setJugador3(char[] jugador3) {
+		this.jugador3 = jugador3;
+	}
+	public char[] getJugador4() {
+		return jugador4;
+	}
+	public void setJugador4(char[] jugador4) {
+		this.jugador4 = jugador4;
+	}
+	public char[] getJugador5() {
+		return jugador5;
+	}
+	public void setJugador5(char[] jugador5) {
+		this.jugador5 = jugador5;
+	}
+	public char[] getJugador6() {
+		return jugador6;
+	}
+	public void setJugador6(char[] jugador6) {
+		this.jugador6 = jugador6;
+	}
+	public char[] getJugador7() {
+		return jugador7;
+	}
+	public void setJugador7(char[] jugador7) {
+		this.jugador7 = jugador7;
+	}
+	public char[] getJugador8() {
+		return jugador8;
+	}
+	public void setJugador8(char[] jugador8) {
+		this.jugador8 = jugador8;
+	}
+	
+	
+	
+}
--- RotarI/src/frmPrincipal.java
+++ RotarI/src/frmPrincipal.java
@@ -17,12 +17,14 @@ import java.util.TimerTask;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class frmPrincipal extends JFrame {
 
 	//Indicador de valor numerico de la primera imagen
 	public static final int IndicePrimeraImagen = 1;	
-	public static final int reinicioContadorFuerza= 0;
+	public static final int reinicioContadorFuerza= -1;
+	public static final char [] palabra = {'B', 'L','A','C','K','_','C','A','T'}; //8 posiciones
 	private JPanel contentPane;	
 	// Panel que contiene Imagen
 	public JPanel panelImagen;
@@ -34,11 +36,31 @@ public class frmPrincipal extends JFrame {
 	public TimerTask timerTask ;
 	
 	public int indiceImagen=0;
+	public int indiceJugadorALlenar= 0;
 	
-	public int contadorFuerza= 0;
+	public int contadorFuerza= -1;
 	
 	Random aleeatorio = new Random();
 	Timer timer ;
+	private JTextField txtJugador1;
+	private JTextField txtJugador2;
+	private JTextField txtJugador3;
+	private JTextField txtJugador4;
+	private JTextField txtJugador5;
+	private JTextField txtJugador6;
+	private JTextField txtJugador7;
+	private JTextField txtJugador8;
+	
+	private int contJugador1 = 1;
+	private int contJugador2=1;
+	private int contJugador3=1;
+	private int contJugador4=1;
+	private int contJugador5=1;
+	private int contJugador6=1;
+	private int contJugador7= 1;
+	private int contJugador8=1;
+	
+  private PuntuacionJugadores puntuacion = new PuntuacionJugadores();
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -65,7 +87,7 @@ public class frmPrincipal extends JFrame {
 		
 		//Inicializa Componentes
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 590, 462);
+		setBounds(100, 100, 785, 484);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -78,7 +100,7 @@ public class frmPrincipal extends JFrame {
 		panelImagen = new JPanel();
 			
 				
-		panelImagen.setBounds(113, 55, 335, 292);
+		panelImagen.setBounds(114, 59, 335, 292);
 							
 		panel.add(panelImagen);
 			
@@ -92,7 +114,7 @@ public class frmPrincipal extends JFrame {
 				iniciaDetiene_AnimacionImagen(true);				
 			}
 		});
-		btnGirarBotella.setBounds(0, 0, 100, 23);
+		btnGirarBotella.setBounds(615, 11, 100, 23);
 		panel.add(btnGirarBotella);
 		
 		JLabel lblJugador1 = new JLabel("Jugador1");
@@ -100,7 +122,7 @@ public class frmPrincipal extends JFrame {
 		panel.add(lblJugador1);
 		
 		JLabel lblJugador3 = new JLabel("Jugador3");
-		lblJugador3.setBounds(458, 182, 59, 14);
+		lblJugador3.setBounds(475, 182, 59, 14);
 		panel.add(lblJugador3);
 		
 		JLabel lblJugador5 = new JLabel("Jugador5");
@@ -116,16 +138,88 @@ public class frmPrincipal extends JFrame {
 		panel.add(lblJugador2);
 		
 		JLabel lblJugador4 = new JLabel("Jugador4");
-		lblJugador4.setBounds(447, 333, 70, 14);
+		lblJugador4.setBounds(464, 337, 70, 14);
 		panel.add(lblJugador4);
 		
 		JLabel lblJugador6 = new JLabel("Jugador6");
-		lblJugador6.setBounds(69, 333, 70, 14);
+		lblJugador6.setBounds(57, 337, 70, 14);
 		panel.add(lblJugador6);
 		
 		JLabel lblJugador8 = new JLabel("Jugador8");
 		lblJugador8.setBounds(69, 34, 70, 14);
 		panel.add(lblJugador8);			
+		
+		JLabel lblJugador = new JLabel("Jugador1: ");
+		lblJugador.setBounds(574, 125, 67, 14);
+		panel.add(lblJugador);
+		
+		JLabel lblJugador_1 = new JLabel("Jugador 2:");
+		lblJugador_1.setBounds(574, 165, 67, 14);
+		panel.add(lblJugador_1);
+		
+		JLabel lblJugador_2 = new JLabel("Jugador 3:");
+		lblJugador_2.setBounds(574, 201, 67, 14);
+		panel.add(lblJugador_2);
+		
+		JLabel lblJugador_3 = new JLabel("Jugador 4:");
+		lblJugador_3.setBounds(574, 238, 67, 14);
+		panel.add(lblJugador_3);
+		
+		JLabel lblJugador_4 = new JLabel("Jugador 5:");
+		lblJugador_4.setBounds(574, 272, 67, 14);
+		panel.add(lblJugador_4);
+		
+		JLabel lblJugador_5 = new JLabel("Jugador 6:");
+		lblJugador_5.setBounds(574, 307, 67, 14);
+		panel.add(lblJugador_5);
+		
+		JLabel lblJugador_6 = new JLabel("Jugador 7:");
+		lblJugador_6.setBounds(574, 337, 67, 14);
+		panel.add(lblJugador_6);
+		
+		JLabel lblJugador_7 = new JLabel("Jugador 8:");
+		lblJugador_7.setBounds(574, 373, 67, 14);
+		panel.add(lblJugador_7);
+		
+		txtJugador1 = new JTextField();
+		txtJugador1.setBounds(642, 122, 86, 20);
+		panel.add(txtJugador1);
+		txtJugador1.setColumns(10);
+		
+		txtJugador2 = new JTextField();
+		txtJugador2.setBounds(642, 162, 86, 20);
+		panel.add(txtJugador2);
+		txtJugador2.setColumns(10);
+		
+		txtJugador3 = new JTextField();
+		txtJugador3.setBounds(642, 198, 86, 20);
+		panel.add(txtJugador3);
+		txtJugador3.setColumns(10);
+		
+		txtJugador4 = new JTextField();
+		txtJugador4.setBounds(642, 235, 86, 20);
+		panel.add(txtJugador4);
+		txtJugador4.setColumns(10);
+		
+		txtJugador5 = new JTextField();
+		txtJugador5.setBounds(642, 269, 86, 20);
+		panel.add(txtJugador5);
+		txtJugador5.setColumns(10);
+		
+		txtJugador6 = new JTextField();
+		txtJugador6.setBounds(642, 304, 86, 20);
+		panel.add(txtJugador6);
+		txtJugador6.setColumns(10);
+		
+		txtJugador7 = new JTextField();
+		txtJugador7.setBounds(642, 334, 86, 20);
+		panel.add(txtJugador7);
+		txtJugador7.setColumns(10);
+		
+		txtJugador8 = new JTextField();
+		txtJugador8.setBounds(642, 370, 86, 20);
+		panel.add(txtJugador8);
+		txtJugador8.setColumns(10);
 		
 		timer = new Timer();
 	}
@@ -143,6 +237,7 @@ public class frmPrincipal extends JFrame {
 		         public void run()  
 		         { 
 		        	 logicaTimer(fuerza);	             
+		        	 
 		         } 
 		     };
 		                             
@@ -169,8 +264,14 @@ public class frmPrincipal extends JFrame {
 		   		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
 		   		 if (fuerza==contadorFuerza) {
+		   			indiceJugadorALlenar++;
 		         contadorFuerza= reinicioContadorFuerza;
+		         contJugador2++; //posición de la palabra a llenar
+		         
+		         txtJugador2.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
+		        
 		   		 iniciaDetiene_AnimacionImagen(verificacion);
+		   		 
 		   		 }
 		   		return ;		
 			case 2:
@@ -179,7 +280,10 @@ public class frmPrincipal extends JFrame {
 		 		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
+   			indiceJugadorALlenar++;
    			contadorFuerza= reinicioContadorFuerza;
+   			puntuacion.llenadoJugadores(indiceImagen, contJugador3, palabra);
+   		   txtJugador3.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		   		 return ;
@@ -189,7 +293,10 @@ public class frmPrincipal extends JFrame {
 		 		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
+   			indiceJugadorALlenar++;
    			contadorFuerza= reinicioContadorFuerza;
+   			puntuacion.llenadoJugadores(indiceImagen, contJugador4, palabra);
+   		   txtJugador4.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		  		 return ;
@@ -199,7 +306,10 @@ public class frmPrincipal extends JFrame {
 		 		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
+   			indiceJugadorALlenar++;
    			contadorFuerza= reinicioContadorFuerza;
+   			puntuacion.llenadoJugadores(indiceImagen, contJugador5, palabra);
+   		   txtJugador5.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		  		 return ;
@@ -209,7 +319,10 @@ public class frmPrincipal extends JFrame {
 		 		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
+   			indiceJugadorALlenar++;
    			contadorFuerza= reinicioContadorFuerza;
+   			puntuacion.llenadoJugadores(indiceImagen, contJugador6, palabra);
+   		   txtJugador6.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		  		return ;
@@ -219,7 +332,10 @@ public class frmPrincipal extends JFrame {
 		 		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
+   			indiceJugadorALlenar++;
    			contadorFuerza= reinicioContadorFuerza;
+   			puntuacion.llenadoJugadores(indiceImagen, contJugador7, palabra);
+   		   txtJugador7.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		  		return ;
@@ -229,7 +345,10 @@ public class frmPrincipal extends JFrame {
 		 		 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
+   			indiceJugadorALlenar++;
    			contadorFuerza= reinicioContadorFuerza;
+			puntuacion.llenadoJugadores(indiceImagen, contJugador8, palabra);
+			   txtJugador8.setText(puntuacion.llenadoJugadores(indiceImagen, contJugador2, palabra));
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		  		 return ;
@@ -240,8 +359,12 @@ public class frmPrincipal extends JFrame {
 				 contadorFuerza++;
 		   		 System.out.println("LA fuerza es: " +contadorFuerza);
    		 if (fuerza==contadorFuerza) {
-   		
+   			 indiceJugadorALlenar++;
+   			
    			contadorFuerza= reinicioContadorFuerza;
+   		
+   			puntuacion.llenadoJugadores(indiceImagen, contJugador1, palabra);
+   		 indiceJugadorALlenar =reinicioContadorFuerza;
 	   		 iniciaDetiene_AnimacionImagen(verificacion);
 		   		 }
 		  		 return ;
